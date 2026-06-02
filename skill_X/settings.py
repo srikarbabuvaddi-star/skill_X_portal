2b1fd0df0c0bb0fc92b28ff993473632a61f4759
srikarbabu <srikarbabuvaddi@gmail.com>
2026-06-02 10:47:02 +0530

    Added gitignore

--- skill_X/settings.py
+++ skill_X/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!8opjhn85d6cl@4t86(wwxv3n2rp&h4vpx$dh&1s@@h50c8szd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
